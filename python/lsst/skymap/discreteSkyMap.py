16f1b04d90f64d3160923fb5df3b5566e7d6b71c
Paul Price <price@astro.princeton.edu>
2013-01-14 18:01:25 -0500

    Use CachingSkyMap and ExplicitTractInfo for DiscreteSkyMap and HealpixSkyMap

--- python/lsst/skymap/discreteSkyMap.py
+++ python/lsst/skymap/discreteSkyMap.py
@@ -23,12 +23,12 @@
 from lsst.pex.config import ListField
 from lsst.afw.coord import IcrsCoord
 import lsst.afw.geom as afwGeom
-from .baseSkyMap import BaseSkyMap
-from .tractInfo import TractInfo
+from .cachingSkyMap import CachingSkyMap
+from .tractInfo import ExplicitTractInfo
 
 __all__ = ["DiscreteSkyMap"]
 
-class DiscreteSkyMapConfig(BaseSkyMap.ConfigClass):
+class DiscreteSkyMapConfig(CachingSkyMap.ConfigClass):
     """Configuration for the DiscreteSkyMap"""
     raList = ListField(dtype=float, default=[], doc="Right Ascensions of tracts (ICRS, degrees)")
     decList = ListField(dtype=float, default=[], doc="Declinations of tracts (ICRS, degrees)")
@@ -43,30 +43,8 @@ class DiscreteSkyMapConfig(BaseSkyMap.ConfigClass):
             raise ValueError("Number of radii (%d) and Decs (%d) do not match" %
                              (len(self.radiusList), len(self.decList)))
 
-class DiscreteTractInfo(TractInfo):
-    """Tract for DiscreteSkyMap
 
-    A tract is placed at the nominated coordinates, with the nominated radius.
-    The tracts are square (i.e., the radius is really a half-size).
-    """
-    def __init__(self, ident, patchInnerDimensions, patchBorder, ctrCoord, tractOverlap, wcs, radius):
-        vertexList = []
-        self._radius = radius
-        super(DiscreteTractInfo, self).__init__(ident, patchInnerDimensions, patchBorder, ctrCoord,
-                                                vertexList, tractOverlap, wcs)
-
-    def _minimumBoundingBox(self, wcs):
-        """The minimum bounding box is calculated using the nominated radius"""
-        bbox = afwGeom.Box2D()
-        for i in range(4):
-            coord = self._ctrCoord.clone()
-            coord.offset(i * 90 * afwGeom.degrees, self._radius)
-            pixPos = wcs.skyToPixel(coord)
-            bbox.include(pixPos)
-        return bbox
-
-
-class DiscreteSkyMap(BaseSkyMap):
+class DiscreteSkyMap(CachingSkyMap):
     """Discrete sky map pixelization.
 
     We put a square Tract at each of the nominated coordinates.
@@ -74,48 +52,22 @@ class DiscreteSkyMap(BaseSkyMap):
     ConfigClass = DiscreteSkyMapConfig
     _version = (1, 0) # for pickle
 
-    def __init__(self, config=None, version=0):
+    def __init__(self, config, version=0):
         """Constructor
 
         @param[in] config: an instance of self.ConfigClass; if None the default config is used
         @param[in] version: software version of this class, to retain compatibility with old instances
         """
-        super(DiscreteSkyMap, self).__init__(config)
-        self._version = version
-        self._numTracts = len(self.config.radiusList)
-        self._tractCache = [None] * self._numTracts
-        self._tractInfo = None # We shouldn't need this; we will generate tracts on demand
+        numTracts = len(config.radiusList)
+        super(DiscreteSkyMap, self).__init__(numTracts, config, version)
 
-    def __reduce__(self):
-        """To support pickling"""
-        return (self.__class__, (self.config, self._version))
-
-    def __getitem__(self, index):
-        """Get the TractInfo for a particular index
-
-        The tract is returned from a cache, if available, otherwise generated
-        on the fly.
-        """
-        if index < 0 or index > self._numTracts:
-            raise IndexError("Index out of range: %d vs %d" % (index, self._numTracts))
-        if self._tractCache[index] is not None:
-            return self._tractCache[index]
+    def generateTract(self, index):
+        """Generate the TractInfo for a particular index"""
         center = IcrsCoord(self.config.raList[index] * afwGeom.degrees,
                            self.config.decList[index] * afwGeom.degrees)
         radius = self.config.radiusList[index]
         wcs = self._wcsFactory.makeWcs(crPixPos=afwGeom.Point2D(0,0), crValCoord=center)
-        tract = DiscreteTractInfo(index, self.config.patchInnerDimensions, self.config.patchBorder, center,
-                                  self.config.tractOverlap * afwGeom.degrees, wcs, radius * afwGeom.degrees)
-        self._tractCache[index] = tract
-        return tract
-
-    def __iter__(self):
-        """Iterator over tracts"""
-        for i in xrange(self._numTracts):
-            yield self[i]
-
-    def __len__(self):
-        """Length is number of tracts"""
-        return self._numTracts
+        return ExplicitTractInfo(index, self.config.patchInnerDimensions, self.config.patchBorder, center,
+                                 radius * afwGeom.degrees, self.config.tractOverlap * afwGeom.degrees, wcs)
 
 
