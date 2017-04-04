a6412d0c781372a6c9fd25ea32e97f16fb40db52
Russell Owen <rowen@uw.edu>
2017-04-04 15:01:34 -0700

    Modernize one test file
    Remove `#!...`
    Add missing `from __future__ import...`
    Update copyright date

--- tests/helper/skyMapTestCase.py
+++ tests/helper/skyMapTestCase.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
-
 #
 # LSST Data Management System
-# Copyright 2008, 2009, 2010, 2012 LSST Corporation.
+# Copyright 2008-2017 LSST Corporation.
 #
 # This product includes software developed by the
 # LSST Project (http://www.lsst.org/).
@@ -21,6 +19,7 @@
 # the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
+from __future__ import absolute_import, division, print_function
 from builtins import zip
 import pickle
 
