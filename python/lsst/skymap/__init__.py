5b4a376d7fecf388e0e74896bf0069a4d9df68fa
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2026-05-08 15:40:24 -0400

    Expose ringsSkyMap

--- python/lsst/skymap/__init__.py
+++ python/lsst/skymap/__init__.py
@@ -5,6 +5,7 @@ from .tractInfo import *
 from .baseSkyMap import *
 from .dodecaSkyMap import *
 from .equatSkyMap import *
+from .ringsSkyMap import *
 from .discreteSkyMap import *
 from .skyMapRegistry import *
 from .version import *
